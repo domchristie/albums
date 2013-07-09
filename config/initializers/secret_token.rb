e61fc8a387dcf69f7b0fe5517de2db5c9f698cca
Dom Christie <christiedom@gmail.com>
2013-07-09 19:21:15 +0100

    Create rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Albums::Application.config.secret_token = '39f562292a877a43c7a337cf6d49976cb5e001beddb7027afd7310c4302f997390e3b9a4b9270573c889dfabe285ce36e47955905841ab03d98f18e4465918d6'
