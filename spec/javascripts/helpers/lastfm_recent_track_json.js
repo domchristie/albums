df56c9eabc3610f27dffe0e438bd32cb0a0f442f
Dom Christie <christiedom@gmail.com>
2013-07-12 11:41:14 +0100

    Improve fixture data to include more albums.

--- spec/javascripts/helpers/lastfm_recent_track_json.js
+++ spec/javascripts/helpers/lastfm_recent_track_json.js
@@ -1,13 +1,193 @@
-lastfmRecentTrackJSON = {
+var lastfmRecentTrackJSON = {
     "recenttracks": {
         "@attr": {
             "page": "1", 
             "perPage": "10", 
-            "total": "12120", 
-            "totalPages": "1212", 
+            "total": "12127", 
+            "totalPages": "1213", 
             "user": "domchristie"
         }, 
         "track": [
+            {
+                "album": {
+                    "#text": "Fourth: The Golden Eagle", 
+                    "mbid": "0048f3fd-8cd2-4880-8428-413daa722203"
+                }, 
+                "artist": {
+                    "#text": "Kelpe", 
+                    "mbid": "270ff0d9-7243-4411-bce5-eeafbefb1b65"
+                }, 
+                "date": {
+                    "#text": "12 Jul 2013, 09:45", 
+                    "uts": "1373622302"
+                }, 
+                "image": [
+                    {
+                        "#text": "http://userserve-ak.last.fm/serve/34s/89661183.jpg", 
+                        "size": "small"
+                    }, 
+                    {
+                        "#text": "http://userserve-ak.last.fm/serve/64s/89661183.jpg", 
+                        "size": "medium"
+                    }, 
+                    {
+                        "#text": "http://userserve-ak.last.fm/serve/126/89661183.jpg", 
+                        "size": "large"
+                    }, 
+                    {
+                        "#text": "http://userserve-ak.last.fm/serve/300x300/89661183.jpg", 
+                        "size": "extralarge"
+                    }
+                ], 
+                "mbid": "", 
+                "name": "Single Stripe (Original Mix)", 
+                "streamable": "0", 
+                "url": "http://www.last.fm/music/Kelpe/_/Single+Stripe+(Original+Mix)"
+            }, 
+            {
+                "album": {
+                    "#text": "Fourth: The Golden Eagle", 
+                    "mbid": "0048f3fd-8cd2-4880-8428-413daa722203"
+                }, 
+                "artist": {
+                    "#text": "Kelpe", 
+                    "mbid": "270ff0d9-7243-4411-bce5-eeafbefb1b65"
+                }, 
+                "date": {
+                    "#text": "12 Jul 2013, 09:39", 
+                    "uts": "1373621997"
+                }, 
+                "image": [
+                    {
+                        "#text": "http://userserve-ak.last.fm/serve/34s/89661183.jpg", 
+                        "size": "small"
+                    }, 
+                    {
+                        "#text": "http://userserve-ak.last.fm/serve/64s/89661183.jpg", 
+                        "size": "medium"
+                    }, 
+                    {
+                        "#text": "http://userserve-ak.last.fm/serve/126/89661183.jpg", 
+                        "size": "large"
+                    }, 
+                    {
+                        "#text": "http://userserve-ak.last.fm/serve/300x300/89661183.jpg", 
+                        "size": "extralarge"
+                    }
+                ], 
+                "mbid": "", 
+                "name": "Nice Eyes in My Size (Original Mix)", 
+                "streamable": "0", 
+                "url": "http://www.last.fm/music/Kelpe/_/Nice+Eyes+in+My+Size+(Original+Mix)"
+            }, 
+            {
+                "album": {
+                    "#text": "Fourth: The Golden Eagle", 
+                    "mbid": "0048f3fd-8cd2-4880-8428-413daa722203"
+                }, 
+                "artist": {
+                    "#text": "Kelpe", 
+                    "mbid": "270ff0d9-7243-4411-bce5-eeafbefb1b65"
+                }, 
+                "date": {
+                    "#text": "12 Jul 2013, 09:35", 
+                    "uts": "1373621728"
+                }, 
+                "image": [
+                    {
+                        "#text": "http://userserve-ak.last.fm/serve/34s/89661183.jpg", 
+                        "size": "small"
+                    }, 
+                    {
+                        "#text": "http://userserve-ak.last.fm/serve/64s/89661183.jpg", 
+                        "size": "medium"
+                    }, 
+                    {
+                        "#text": "http://userserve-ak.last.fm/serve/126/89661183.jpg", 
+                        "size": "large"
+                    }, 
+                    {
+                        "#text": "http://userserve-ak.last.fm/serve/300x300/89661183.jpg", 
+                        "size": "extralarge"
+                    }
+                ], 
+                "mbid": "", 
+                "name": "Go Visible (Original Mix)", 
+                "streamable": "0", 
+                "url": "http://www.last.fm/music/Kelpe/_/Go+Visible+(Original+Mix)"
+            }, 
+            {
+                "album": {
+                    "#text": "Fourth: The Golden Eagle", 
+                    "mbid": "0048f3fd-8cd2-4880-8428-413daa722203"
+                }, 
+                "artist": {
+                    "#text": "Kelpe", 
+                    "mbid": "270ff0d9-7243-4411-bce5-eeafbefb1b65"
+                }, 
+                "date": {
+                    "#text": "12 Jul 2013, 09:32", 
+                    "uts": "1373621549"
+                }, 
+                "image": [
+                    {
+                        "#text": "http://userserve-ak.last.fm/serve/34s/89661183.jpg", 
+                        "size": "small"
+                    }, 
+                    {
+                        "#text": "http://userserve-ak.last.fm/serve/64s/89661183.jpg", 
+                        "size": "medium"
+                    }, 
+                    {
+                        "#text": "http://userserve-ak.last.fm/serve/126/89661183.jpg", 
+                        "size": "large"
+                    }, 
+                    {
+                        "#text": "http://userserve-ak.last.fm/serve/300x300/89661183.jpg", 
+                        "size": "extralarge"
+                    }
+                ], 
+                "mbid": "", 
+                "name": "Superzero Theme (Original Mix)", 
+                "streamable": "0", 
+                "url": "http://www.last.fm/music/Kelpe/_/Superzero+Theme+(Original+Mix)"
+            }, 
+            {
+                "album": {
+                    "#text": "Fourth: The Golden Eagle", 
+                    "mbid": "0048f3fd-8cd2-4880-8428-413daa722203"
+                }, 
+                "artist": {
+                    "#text": "Kelpe", 
+                    "mbid": "270ff0d9-7243-4411-bce5-eeafbefb1b65"
+                }, 
+                "date": {
+                    "#text": "12 Jul 2013, 09:27", 
+                    "uts": "1373621277"
+                }, 
+                "image": [
+                    {
+                        "#text": "http://userserve-ak.last.fm/serve/34s/89661183.jpg", 
+                        "size": "small"
+                    }, 
+                    {
+                        "#text": "http://userserve-ak.last.fm/serve/64s/89661183.jpg", 
+                        "size": "medium"
+                    }, 
+                    {
+                        "#text": "http://userserve-ak.last.fm/serve/126/89661183.jpg", 
+                        "size": "large"
+                    }, 
+                    {
+                        "#text": "http://userserve-ak.last.fm/serve/300x300/89661183.jpg", 
+                        "size": "extralarge"
+                    }
+                ], 
+                "mbid": "", 
+                "name": "Beaks of Eagles (Original Mix)", 
+                "streamable": "0", 
+                "url": "http://www.last.fm/music/Kelpe/_/Beaks+of+Eagles+(Original+Mix)"
+            }, 
             {
                 "album": {
                     "#text": "Welcome To Mali", 
@@ -187,187 +367,7 @@ lastfmRecentTrackJSON = {
                 "name": "Bozos (Album Version)", 
                 "streamable": "0", 
                 "url": "http://www.last.fm/music/Amadou+&+Mariam/_/Bozos+(Album+Version)"
-            }, 
-            {
-                "album": {
-                    "#text": "Welcome To Mali", 
-                    "mbid": "329fa3bc-b408-45a9-905f-ad243ca7b63d"
-                }, 
-                "artist": {
-                    "#text": "Amadou & Mariam", 
-                    "mbid": "1fb60b6c-0f4d-42b4-8a5e-de705ec76660"
-                }, 
-                "date": {
-                    "#text": "11 Jul 2013, 20:17", 
-                    "uts": "1373573841"
-                }, 
-                "image": [
-                    {
-                        "#text": "http://userserve-ak.last.fm/serve/34s/24342917.png", 
-                        "size": "small"
-                    }, 
-                    {
-                        "#text": "http://userserve-ak.last.fm/serve/64s/24342917.png", 
-                        "size": "medium"
-                    }, 
-                    {
-                        "#text": "http://userserve-ak.last.fm/serve/126/24342917.png", 
-                        "size": "large"
-                    }, 
-                    {
-                        "#text": "http://userserve-ak.last.fm/serve/300x300/24342917.png", 
-                        "size": "extralarge"
-                    }
-                ], 
-                "mbid": "", 
-                "name": "Unissons Nous [feat. Keziah Jones] (Album Version)", 
-                "streamable": "0", 
-                "url": "http://www.last.fm/music/Amadou+&+Mariam/_/Unissons+Nous+%5Bfeat.+Keziah+Jones%5D+(Album+Version)"
-            }, 
-            {
-                "album": {
-                    "#text": "Welcome To Mali", 
-                    "mbid": "329fa3bc-b408-45a9-905f-ad243ca7b63d"
-                }, 
-                "artist": {
-                    "#text": "Amadou & Mariam", 
-                    "mbid": "1fb60b6c-0f4d-42b4-8a5e-de705ec76660"
-                }, 
-                "date": {
-                    "#text": "11 Jul 2013, 20:13", 
-                    "uts": "1373573615"
-                }, 
-                "image": [
-                    {
-                        "#text": "http://userserve-ak.last.fm/serve/34s/24342917.png", 
-                        "size": "small"
-                    }, 
-                    {
-                        "#text": "http://userserve-ak.last.fm/serve/64s/24342917.png", 
-                        "size": "medium"
-                    }, 
-                    {
-                        "#text": "http://userserve-ak.last.fm/serve/126/24342917.png", 
-                        "size": "large"
-                    }, 
-                    {
-                        "#text": "http://userserve-ak.last.fm/serve/300x300/24342917.png", 
-                        "size": "extralarge"
-                    }
-                ], 
-                "mbid": "", 
-                "name": "Compagnon de la Vie (Album Version)", 
-                "streamable": "0", 
-                "url": "http://www.last.fm/music/Amadou+&+Mariam/_/Compagnon+de+la+Vie+(Album+Version)"
-            }, 
-            {
-                "album": {
-                    "#text": "Welcome To Mali", 
-                    "mbid": "329fa3bc-b408-45a9-905f-ad243ca7b63d"
-                }, 
-                "artist": {
-                    "#text": "Amadou & Mariam", 
-                    "mbid": "1fb60b6c-0f4d-42b4-8a5e-de705ec76660"
-                }, 
-                "date": {
-                    "#text": "11 Jul 2013, 20:09", 
-                    "uts": "1373573386"
-                }, 
-                "image": [
-                    {
-                        "#text": "http://userserve-ak.last.fm/serve/34s/24342917.png", 
-                        "size": "small"
-                    }, 
-                    {
-                        "#text": "http://userserve-ak.last.fm/serve/64s/24342917.png", 
-                        "size": "medium"
-                    }, 
-                    {
-                        "#text": "http://userserve-ak.last.fm/serve/126/24342917.png", 
-                        "size": "large"
-                    }, 
-                    {
-                        "#text": "http://userserve-ak.last.fm/serve/300x300/24342917.png", 
-                        "size": "extralarge"
-                    }
-                ], 
-                "mbid": "1f1f947c-08b8-4f32-8208-71da42b2bef0", 
-                "name": "Africa", 
-                "streamable": "0", 
-                "url": "http://www.last.fm/music/Amadou+&+Mariam/_/Africa"
-            }, 
-            {
-                "album": {
-                    "#text": "Welcome To Mali", 
-                    "mbid": "329fa3bc-b408-45a9-905f-ad243ca7b63d"
-                }, 
-                "artist": {
-                    "#text": "Amadou & Mariam", 
-                    "mbid": "1fb60b6c-0f4d-42b4-8a5e-de705ec76660"
-                }, 
-                "date": {
-                    "#text": "11 Jul 2013, 20:05", 
-                    "uts": "1373573150"
-                }, 
-                "image": [
-                    {
-                        "#text": "http://userserve-ak.last.fm/serve/34s/24342917.png", 
-                        "size": "small"
-                    }, 
-                    {
-                        "#text": "http://userserve-ak.last.fm/serve/64s/24342917.png", 
-                        "size": "medium"
-                    }, 
-                    {
-                        "#text": "http://userserve-ak.last.fm/serve/126/24342917.png", 
-                        "size": "large"
-                    }, 
-                    {
-                        "#text": "http://userserve-ak.last.fm/serve/300x300/24342917.png", 
-                        "size": "extralarge"
-                    }
-                ], 
-                "mbid": "", 
-                "name": "Masiteladi (Featuring M) (Album Ver.)", 
-                "streamable": "0", 
-                "url": "http://www.last.fm/music/Amadou+&+Mariam/_/Masiteladi+(Featuring+M)+(Album+Ver.)"
-            }, 
-            {
-                "album": {
-                    "#text": "Welcome To Mali", 
-                    "mbid": "329fa3bc-b408-45a9-905f-ad243ca7b63d"
-                }, 
-                "artist": {
-                    "#text": "Amadou & Mariam", 
-                    "mbid": "1fb60b6c-0f4d-42b4-8a5e-de705ec76660"
-                }, 
-                "date": {
-                    "#text": "11 Jul 2013, 20:01", 
-                    "uts": "1373572892"
-                }, 
-                "image": [
-                    {
-                        "#text": "http://userserve-ak.last.fm/serve/34s/24342917.png", 
-                        "size": "small"
-                    }, 
-                    {
-                        "#text": "http://userserve-ak.last.fm/serve/64s/24342917.png", 
-                        "size": "medium"
-                    }, 
-                    {
-                        "#text": "http://userserve-ak.last.fm/serve/126/24342917.png", 
-                        "size": "large"
-                    }, 
-                    {
-                        "#text": "http://userserve-ak.last.fm/serve/300x300/24342917.png", 
-                        "size": "extralarge"
-                    }
-                ], 
-                "mbid": "", 
-                "name": "Je Te Kiffe [feat. Juan Rozoff] (Album Version)", 
-                "streamable": "0", 
-                "url": "http://www.last.fm/music/Amadou+&+Mariam/_/Je+Te+Kiffe+%5Bfeat.+Juan+Rozoff%5D+(Album+Version)"
             }
         ]
     }
-};
+}
--- spec/javascripts/tracks_collection.spec.js
+++ spec/javascripts/tracks_collection.spec.js
@@ -8,7 +8,7 @@ describe('TracksCollection', function() {
       var tracksCollection = new Albums.TracksCollection(),
           parsed = tracksCollection.parse(lastfmRecentTrackJSON);
       expect(parsed.length).toEqual(10);
-      expect(parsed[0].name).toEqual("Sebeke (Album Ver.)");
+      expect(parsed[0].name).toEqual("Single Stripe (Original Mix)");
     });
   });
 });
