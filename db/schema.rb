a02a476878c1da589ac4c35db6f6d6df3ab45846
Dom Christie <christiedom@gmail.com>
2013-07-12 15:40:14 +0100

    Add sorcery external

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130712134945) do
+ActiveRecord::Schema.define(:version => 20130712143739) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id",    :null => false
