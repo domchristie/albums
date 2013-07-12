984c38ad3a626e1fbaad2256a1f120b5f100e901
Dom Christie <christiedom@gmail.com>
2013-07-12 15:46:45 +0100

    Configure sorcery external submodule

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -70,8 +70,12 @@ Rails.application.config.sorcery.configure do |config|
   # What providers are supported by this app, i.e. [:twitter, :facebook, :github, :linkedin, :xing, :google, :liveid] .
   # Default: `[]`
   #
-  # config.external_providers =
-
+  config.external_providers = [:twitter]
+      
+  config.twitter.key = ENV['TWITTER_KEY']
+  config.twitter.secret = ENV['TWITTER_SECRET']
+  config.twitter.callback_url = ENV['TWITTER_CALLBACK_URL']
+  config.twitter.user_info_mapping = { username: "screen_name" }
 
   # You can change it by your local ca_file. i.e. '/etc/pki/tls/certs/ca-bundle.crt'
   # Path to ca_file. By default use a internal ca-bundle.crt.
@@ -410,8 +414,7 @@ Rails.application.config.sorcery.configure do |config|
     # Class which holds the various external provider data for this user.
     # Default: `nil`
     #
-    # user.authentications_class =
-
+    user.authentications_class = Authentication
 
     # User's identifier in authentications class.
     # Default: `:user_id`
