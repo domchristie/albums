0f5b1a93365fcec36bdedcc1ce3ca083cb9d606e
Dom Christie <christiedom@gmail.com>
2013-07-15 20:48:35 +0100

    Set current user on the client-side

--- app/assets/javascripts/albums.js
+++ app/assets/javascripts/albums.js
@@ -8,6 +8,10 @@ window.Albums = {
   init: function(options) {
     this.router = new Albums.Router();
     this.tracks = new Albums.TracksCollection();
+    if(options && options.currentUser) {
+      this.currentUser = new Albums.User(options.currentUser);
+    }
+    
     new Albums.SearchView({ model: new Albums.Search() });
     Backbone.history.start();
   }
---
+++ app/assets/javascripts/models/user.js
@@ -0,0 +1,1 @@
+Albums.User = Backbone.Model.extend();
