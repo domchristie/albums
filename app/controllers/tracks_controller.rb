62218190b4765575ec7e9e49f1f192d8bddd5ec4
Dom Christie <christiedom@gmail.com>
2013-07-15 20:54:30 +0100

    Move last.fm api call into json block

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,13 +1,14 @@
 class TracksController < ApplicationController
   def index
-    base = "http://ws.audioscrobbler.com/2.0/"
-    username = params[:username]
-    uri = URI.parse("#{base}?method=user.getrecenttracks&user=#{username}&api_key=#{ENV["LAST_FM_API_KEY"]}&format=json")
-    response = Net::HTTP.get_response(uri)
-    
     respond_to do |format|
       format.html {}
-      format.json { render json: response.body }
+      format.json {
+        base = "http://ws.audioscrobbler.com/2.0/"
+        username = params[:username]
+        uri = URI.parse("#{base}?method=user.getrecenttracks&user=#{username}&limit=#{limit}&api_key=#{ENV["LAST_FM_API_KEY"]}&format=json")
+        response = Net::HTTP.get_response(uri)
+        render json: response.body
+      }
     end
 
   end
