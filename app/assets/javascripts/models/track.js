2e9f188cec477870b97a91340c7a906f93b36043
Dom Christie <christiedom@gmail.com>
2013-08-08 23:27:28 +0100

    Use identicon if no image present

--- app/assets/javascripts/models/track.js
+++ app/assets/javascripts/models/track.js
@@ -1,12 +1,13 @@
 Albums.Track = Backbone.Model.extend({
   initialize: function() {
+    var artistName = this.get('artist')['#text'];
+    this.set('artistName', artistName);
+    
     var imageUrl = _.find(this.get('image'), function(image) {
       return image.size === 'extralarge';
     })['#text'];
-    this.set('imageUrl', imageUrl);
-    
-    var artistName = this.get('artist')['#text'];
-    this.set('artistName', artistName);
+    this.set('imageUrl', imageUrl ||
+      "http://www.gravatar.com/avatar/" + hex_md5(this.get('album')['#text']) + "?f=y&d=identicon&s=300");
   }
 });
 
